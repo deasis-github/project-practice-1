562e9853697dcef7be3f0740b3a8f793c1d60d61
deasis-github <deasis.it.dev@gmail.com>
2022-10-03 08:34:17 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
+ 
 function App() {
+ 
+   const [usersList, setUsersList] = useState([]);
+   const addUserHandler = (uName, uAge) => {
+      setUsersList((prevUsersList)=> {
+         return [...prevUsersList, {id: Math.random().toString(), name: uName, age: uAge}]
+      })
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+        <AddUser onAddUser={addUserHandler}/>
+        <UserList users={usersList}/>
+     </div>
   );
 }
 
